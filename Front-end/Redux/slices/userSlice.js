b0fc50e2d60bdb7ff2e795758dbda6b4ddefb9fe
sumit sharma <sumitshar9340@gmail.com>
2025-05-22 21:43:56 +0530

    Update userSlice.js

--- Front-end/Redux/slices/userSlice.js
+++ Front-end/Redux/slices/userSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000';
+const API_URL = 'https://note-vault-hiiy.onrender.com';
 
 export const updateUser = createAsyncThunk('user/updateUser', async (formData, { getState, rejectWithValue }) => {
   try {
